d8c4bf4b6b4d0029e5351bd643f4d34f6b54ba69
Voyen <dev@voyen.io>
2020-11-25 18:35:53 +1030

    updated startup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 ENV_CHECK=$(grep -e '=$' .env | wc -l)
 if [ 0 -eq $ENV_CHECK ]; then
   export $(cat .env | xargs)
+  [ "$1" == "start" ] && ./mvnw spring-boot:run
 else
   echo 'Missing API key. Please update .env file appropriately.'
 fi
